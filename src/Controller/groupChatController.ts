440e26a1be6481ae9742254c6de43a545aa7e4e7
OutsydBaby <amichdot@gmail.com>
2025-11-25 18:01:36 +0100

    Implemented addUser feature

--- src/Controller/groupChatController.ts
+++ src/Controller/groupChatController.ts
@@ -38,3 +38,44 @@ export const createGroup = async (req: Request, res: Response) => {
         })
     }
 }
+
+export const addUser = async (req: Request, res: Response) => {
+    try {
+        const {chatId, userId} = req.body;
+        const loggedInUser = (req as any).user._id
+
+        if(!chatId && !userId) {
+            return res.status(404).json({message: 'chatId and userId are required'});
+        }
+        // check if chat exist
+        const chatExist = await Chat.findById(chatId);
+        if(!chatExist) {
+            return res.status(404).json({ message: 'Chat not found'})
+        }
+
+        // Ensure its a group chat
+        if(!chatExist.isGroupChat){
+            return res.status(400).json({message: 'Can only add users to group chats and not direct chats'})
+        }
+        // Only group admin can add users
+        if(chatExist.groupAdmin.toString() != loggedInUser.toString()){
+            return res.status(400).json({message: 'Only group admin can add users to group'})
+        }
+        // Add new user
+        chatExist.users.push(userId)
+        await chatExist.save();
+
+        let fullChat = await chatExist.populate('users', 'username email');
+        res.status(200).json({
+            status: 'success',
+            chat: fullChat,
+            message: 'User added to the group'
+        })
+
+    } catch (error) {
+        res.status(400).json({
+            status: 'fail',
+            message: 'Error adding new user to the group', error
+        })
+    }
+}
