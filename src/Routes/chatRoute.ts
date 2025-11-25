df98a273effe94aefedfc17def3e5b92e5a35fbe
OutsydBaby <amichdot@gmail.com>
2025-11-25 16:00:10 +0100

    Added createGroup to chatRoute

--- src/Routes/chatRoute.ts
+++ src/Routes/chatRoute.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import { protect } from '../Middleware/protect'
 import { accessChat, getChat } from '../Controller/chatController';
+import { createGroup } from '../Controller/groupChatController';
 
 const router = express.Router();
 router
     .post('/', protect, accessChat)
+    .post('/group', protect, createGroup)
     .get('/', protect, getChat)
 
 export default router;
