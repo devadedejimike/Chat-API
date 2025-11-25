ac3818627aea80bf3076c3de92d8b756132f362f
OutsydBaby <amichdot@gmail.com>
2025-11-25 18:38:55 +0100

    Added addUser & removeUser route

--- src/Routes/chatRoute.ts
+++ src/Routes/chatRoute.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import { protect } from '../Middleware/protect'
 import { accessChat, getChat } from '../Controller/chatController';
-import { createGroup } from '../Controller/groupChatController';
+import { createGroup, addUser, removeUser } from '../Controller/groupChatController';
 
 const router = express.Router();
 router
     .post('/', protect, accessChat)
     .post('/group', protect, createGroup)
+    .post('/add', protect, addUser)
+    .post('/remove', protect, removeUser)
     .get('/', protect, getChat)
 
 export default router;
