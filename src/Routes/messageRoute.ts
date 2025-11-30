3a67f5951ce42e6f5a1c6d1f96a72aed320b845e
OutsydBaby <amichdot@gmail.com>
2025-11-30 04:18:21 +0100

    Updated message route to be able to upload files

--- src/Routes/messageRoute.ts
+++ src/Routes/messageRoute.ts
@@ -1,12 +1,14 @@
 import { sendMessage, fetchMessages, deleteMessage, searchMessage } from "../Controller/messageController";
 import { protect } from "../Middleware/protect";
 import express from 'express';
+import multer from "multer";
+const upload = multer();
 
 
 const router = express.Router();
 
 router
-    .post('/send', protect, sendMessage)
+    .post('/send', protect, upload.single("file"), sendMessage as unknown as express.RequestHandler)
     .get('/:chatId', protect, fetchMessages)
     .get('/search/:chatId', protect, searchMessage)
     .delete('/delete', protect, deleteMessage)
